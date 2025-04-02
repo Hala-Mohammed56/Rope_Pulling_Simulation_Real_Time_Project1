6f712409240bc64311f79808144b9f8242195c48
Hala Mohammed <halaabed56@gmail.com>
2025-04-02 23:38:16 +0200

    Update header.h

--- header.h
+++ header.h
@@ -1,8 +1,7 @@
-/* Updated: header.h centralized for main.c and player.c */
-
 #ifndef HEADER_H
 #define HEADER_H
 
+// Standard libraries
 #include <stdio.h>
 #include <stdlib.h>
 #include <unistd.h>
@@ -10,17 +9,21 @@
 #include <time.h>
 #include <string.h>
 #include <fcntl.h>
-#include <sys/wait.h>
+#include <errno.h>
+#include <limits.h>
+
+// Process & IPC
 #include <sys/types.h>
 #include <sys/stat.h>
-#include <limits.h>
+#include <sys/wait.h>
 #include <sys/ipc.h>
 #include <sys/msg.h>
 #include <sys/sem.h>
+#include <sys/shm.h>
 #include <semaphore.h>
 #include <pthread.h>
-#include <sys/shm.h>
-#include <errno.h>
+
+// OpenGL
 #include <GL/glut.h>
 
 #endif
