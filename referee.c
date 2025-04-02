fcee060ebc16f6957186aee69d18322f94067b2a
Jenan Mohammad <153653638+jenanmashhour12@users.noreply.github.com>
2025-04-02 06:13:22 +0200

    Fixed_Referee.c

--- referee.c
+++ referee.c
@@ -1,180 +1,163 @@
-#include "header.h"
-#include <sys/wait.h>
-#include <fcntl.h>
-#include <errno.h>
 
-#define NUM_PLAYERS 8
+#include <stdio.h>
+#include <stdlib.h>
+#include <unistd.h>
+#include <signal.h>
+#include <sys/wait.h>
+#include <time.h>
+
 #define TEAM_SIZE 4
+#define NUM_PLAYERS 8
+
+#define RED     "\033[1;31m"
+#define GREEN   "\033[1;32m"
+#define YELLOW  "\033[1;33m"
+#define CYAN    "\033[1;36m"
+#define RESET   "\033[0m"
 
 typedef struct {
-    int energy_min;
-    int energy_max;
-    int effort_decrease_min;
-    int effort_decrease_max;
-    int recovery_time_min;
-    int recovery_time_max;
+    int energy_min, energy_max;
+    int decrease_min, decrease_max;
+    int recovery_min, recovery_max;
     int win_threshold;
     int game_duration;
     int rounds_to_win;
 } GameConfig;
 
 GameConfig config;
+pid_t players[NUM_PLAYERS];
 
-void read_config(const char *filename) {
-    FILE *fp = fopen(filename, "r");
-    if (!fp) {
+void read_config(const char* filename) {
+    FILE* file = fopen(filename, "r");
+    if (!file) {
         perror("Failed to open config file");
-        exit(1);
+        exit(EXIT_FAILURE);
     }
 
-    fscanf(fp, "%d %d", &config.energy_min, &config.energy_max);
-    fscanf(fp, "%d %d", &config.effort_decrease_min, &config.effort_decrease_max);
-    fscanf(fp, "%d %d", &config.recovery_time_min, &config.recovery_time_max);
-    fscanf(fp, "%d", &config.win_threshold);
-    fscanf(fp, "%d", &config.game_duration);
-    fscanf(fp, "%d", &config.rounds_to_win);
-
-    fclose(fp);
+    fscanf(file, "%d %d %d %d %d %d %d %d %d",
+           &config.energy_min, &config.energy_max,
+           &config.decrease_min, &config.decrease_max,
+           &config.recovery_min, &config.recovery_max,
+           &config.win_threshold,
+           &config.game_duration,
+           &config.rounds_to_win);
+    fclose(file);
 }
 
-void assign_positions(int energies[], int positions[]) {
-    int sorted_indices[TEAM_SIZE];
-    for (int i = 0; i < TEAM_SIZE; i++) {
-        sorted_indices[i] = i;
-    }
-
-    for (int i = 0; i < TEAM_SIZE - 1; i++) {
-        for (int j = i + 1; j < TEAM_SIZE; j++) {
-            if (energies[sorted_indices[i]] > energies[sorted_indices[j]]) {
-                int tmp = sorted_indices[i];
-                sorted_indices[i] = sorted_indices[j];
-                sorted_indices[j] = tmp;
+void assign_positions(int efforts[], int positions[]) {
+    int order[TEAM_SIZE] = {0, 1, 2, 3};
+    for (int i = 0; i < TEAM_SIZE-1; i++) {
+        for (int j = i+1; j < TEAM_SIZE; j++) {
+            if (efforts[order[i]] > efforts[order[j]]) {
+                int tmp = order[i];
+                order[i] = order[j];
+                order[j] = tmp;
             }
         }
     }
-
     for (int i = 0; i < TEAM_SIZE; i++) {
-        positions[sorted_indices[i]] = i;  // position: 0 (lowest) to 3 (highest)
-    }
-
-    // Debug print
-    printf("\n Team Energies & Assigned Positions:\n");
-    for (int i = 0; i < TEAM_SIZE; i++) {
-        printf("Player %d → Energy = %d → Position = %d\n", i, energies[i], positions[i]);
+        positions[order[i]] = i + 1;
     }
 }
 
-int main(int argc, char *argv[]) {
-    if (argc < 2) {
-        printf("Usage: %s config.txt\n", argv[0]);
+int main(int argc, char* argv[]) {
+    if (argc != 2) {
+        fprintf(stderr, "Usage: %s <config_file>\n", argv[0]);
         return 1;
     }
 
+     // log to file
     read_config(argv[1]);
+    int read_pipes[NUM_PLAYERS][2], write_pipes[NUM_PLAYERS][2];
 
-    int team1_pipe[2], team2_pipe[2];
-    pipe(team1_pipe);
-    pipe(team2_pipe);
+    for (int i = 0; i < NUM_PLAYERS; i++) {
+        pipe(read_pipes[i]);
+        pipe(write_pipes[i]);
+    }
 
-    pid_t players_pids[NUM_PLAYERS];
-    int team1_energies[TEAM_SIZE];
-    int team2_energies[TEAM_SIZE];
-    int team1_positions[TEAM_SIZE];
-    int team2_positions[TEAM_SIZE];
-
-
-    for (int player_id = 0; player_id < NUM_PLAYERS; player_id++) {
-        pid_t child_pid = fork();
-
-        if (child_pid < 0) {
-            perror("Error: fork failed");
-            exit(1);
-        }
-
-        if (child_pid == 0) {
-            for (int j = 0; j < 2; j++) {
-                if (player_id < 4) close(team1_pipe[0]);
-                else close(team2_pipe[0]);
+    for (int i = 0; i < NUM_PLAYERS; i++) {
+        players[i] = fork();
+        if (players[i] == 0) {
+            for (int j = 0; j < NUM_PLAYERS; j++) {
+                if (j != i) {
+                    close(read_pipes[j][0]); close(read_pipes[j][1]);
+                    close(write_pipes[j][0]); close(write_pipes[j][1]);
+                }
             }
 
-            char position_str[4];
-            char pipe_fd_str[4];
-            sprintf(position_str, "%d", player_id % 4);
-            sprintf(pipe_fd_str, "%d", player_id < 4 ? team1_pipe[1] : team2_pipe[1]);
-            execl("./player", "player", position_str, pipe_fd_str, argv[1], NULL);
+            char pos[10], rfd[10], wfd[10];
+            sprintf(pos, "%d", i % TEAM_SIZE);
+            sprintf(rfd, "%d", write_pipes[i][0]);
+            sprintf(wfd, "%d", read_pipes[i][1]);
+
+            execl("./player", "player", pos, rfd, wfd, argv[1], NULL);
             perror("execl failed");
             exit(1);
-        } else {
-            players_pids[player_id] = child_pid;
         }
-    }
-
-
-    printf("\nAll player processes created successfully:\n");
-    for (int i = 0; i < NUM_PLAYERS; i++) {
-        printf("Player %d → PID: %d\n", i, players_pids[i]);
-    }
-
-    sleep(1);
-    printf("\nReferee: Sending SIGUSR1 to all players (Get Ready)...\n");
-    for (int i = 0; i < NUM_PLAYERS; i++) kill(players_pids[i], SIGUSR1);
-    sleep(1);
-
-    printf("\nReferee: Sending SIGUSR2 to all players (Start Pulling)...\n");
-    for (int i = 0; i < NUM_PLAYERS; i++) kill(players_pids[i], SIGUSR2);
-    sleep(1);
-
-    printf("\nReferee: Reading efforts from players...\n");
-
-    int team1_total = 0, team2_total = 0;
-    int effort;
-
-    for (int i = 0; i < TEAM_SIZE; i++) {
-        if (read(team1_pipe[0], &effort, sizeof(int)) > 0) {
-            team1_energies[i] = effort;
-            printf("Team 1 - Player %d effort: %d\n", i, effort);
-            team1_total += effort;
-        } else {
-            perror("Failed to read from team1_pipe");
-        }
-    }
-
-    for (int i = 0; i < TEAM_SIZE; i++) {
-        if (read(team2_pipe[0], &effort, sizeof(int)) > 0) {
-            team2_energies[i] = effort;
-            printf("Team 2 - Player %d effort: %d\n", i + 4, effort);
-            team2_total += effort;
-        } else {
-            perror("Failed to read from team2_pipe");
-        }
-    }
-
-    assign_positions(team1_energies, team1_positions);
-    assign_positions(team2_energies, team2_positions);
-
-    // Send position to each player via pipe
-    for (int i = 0; i < TEAM_SIZE; i++) {
-        write(team1_pipe[1], &team1_positions[i], sizeof(int));
-    }
-    for (int i = 0; i < TEAM_SIZE; i++) {
-        write(team2_pipe[1], &team2_positions[i], sizeof(int));
-    }
-    close(team1_pipe[1]);
-    close(team2_pipe[1]);
-
-    printf("\nTeam 1 Total Effort: %d\n", team1_total);
-    printf("Team 2 Total Effort: %d\n", team2_total);
-
-    if (team1_total > team2_total) {
-        printf("************Team 1 wins this round!************\n");
-    } else if (team2_total > team1_total) {
-        printf("************Team 2 wins this round!************\n");
-    } else {
-        printf("************It's a tie!************\n");
     }
 
     for (int i = 0; i < NUM_PLAYERS; i++) {
-        kill(players_pids[i], SIGKILL);
+        close(read_pipes[i][1]);
+        close(write_pipes[i][0]);
+    }
+
+    sleep(1);
+    int score1 = 0, score2 = 0;
+
+    for (int round = 1; round <= config.rounds_to_win; round++) {
+        printf("\n=== Round %d ===\n", round);
+        usleep(500000);
+
+        for (int i = 0; i < NUM_PLAYERS; i++) kill(players[i], SIGUSR1);
+        sleep(1);
+
+        int team1_pos[TEAM_SIZE], team2_pos[TEAM_SIZE];
+        int team1_eff[TEAM_SIZE], team2_eff[TEAM_SIZE];
+
+        assign_positions((int[4]){1, 2, 3, 4}, team1_pos);
+        assign_positions((int[4]){1, 2, 3, 4}, team2_pos);
+
+        for (int i = 0; i < TEAM_SIZE; i++) {
+            write(write_pipes[i][1], &team1_pos[i], sizeof(int));
+            write(write_pipes[i + TEAM_SIZE][1], &team2_pos[i], sizeof(int));
+        }
+
+        for (int i = 0; i < NUM_PLAYERS; i++) kill(players[i], SIGUSR2);
+        sleep(1);
+
+        int total1 = 0, total2 = 0;
+        for (int i = 0; i < TEAM_SIZE; i++) {
+            int effort1, effort2;
+            read(read_pipes[i][0], &effort1, sizeof(int));
+            read(read_pipes[i + TEAM_SIZE][0], &effort2, sizeof(int));
+            total1 += effort1;
+            total2 += effort2;
+            printf("Player T1-P%d: Effort=%d\t| Player T2-P%d: Effort=%d\n", i, effort1, i, effort2);
+        }
+
+        printf(">> Team 1 Total: %d | Team 2 Total: %d\n", total1, total2);
+        if (total1 > total2) {
+            printf("🏅 Round %d Winner: Team 1\n", round);
+            score1++;
+        } else if (total2 > total1) {
+            printf("🏅 Round %d Winner: Team 2\n", round);
+            score2++;
+        } else {
+            printf("🤝 Round %d is a tie!\n", round);
+        }
+        usleep(500000);
+    }
+
+    printf("\n=== Game Over ===\n");
+    if (score1 > score2)
+        printf("🏆 Final Winner: Team 1!\n");
+    else if (score2 > score1)
+        printf("🏆 Final Winner: Team 2!\n");
+    else
+        printf("🏁 Final Result: It's a tie!\n");
+
+    for (int i = 0; i < NUM_PLAYERS; i++) {
+        kill(players[i], SIGTERM);
         wait(NULL);
     }
 
