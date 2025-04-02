fcc3d4dc9b648b6be9ed8347ba70d8bcc384d402
Hala Mohammed <halaabed56@gmail.com>
2025-04-02 23:37:31 +0200

    Update Fixed_Referee.c

--- Fixed_Referee.c
+++ Fixed_Referee.c
@@ -1,39 +1,15 @@
-#include <stdio.h>
-#include <stdlib.h>
-#include <unistd.h>
-#include <signal.h>
-#include <sys/wait.h>
-#include <time.h>
-#include <string.h>
+#include "header.h"
+#include "constants.h"
+#include "structs.h"
 
-#define TEAM_SIZE 4
-#define NUM_PLAYERS 8
-
-#define RED     "\033[1;31m"
-#define GREEN   "\033[1;32m"
-#define YELLOW  "\033[1;33m"
-#define CYAN    "\033[1;36m"
-#define RESET   "\033[0m"
-
-typedef struct {
-    int energy_min, energy_max;
-    int decrease_min, decrease_max;
-    int recovery_min, recovery_max;
-    int win_threshold;
-    int game_duration;
-    int rounds_to_win;
-} GameConfig;
-
+// Global configuration and players array
 GameConfig config;
 pid_t players[NUM_PLAYERS];
 
-typedef struct {
-    int player_id;
-    int position;
-    int energy;
-    int effort;
-} PlayerStats;
 
+// ##################################
+// Loads game configuration values from a file provided by the user
+// ##################################
 void read_config(const char* filename) {
     FILE* file = fopen(filename, "r");
     if (!file) {
@@ -51,6 +27,10 @@ void read_config(const char* filename) {
     fclose(file);
 }
 
+
+// ##################################
+// Gives player positions based on their current energy levels
+// ##################################
 void assign_positions(int energies[], int positions[]) {
     int order[TEAM_SIZE] = {0, 1, 2, 3};
     for (int i = 0; i < TEAM_SIZE-1; i++) {
@@ -67,23 +47,31 @@ void assign_positions(int energies[], int positions[]) {
     }
 }
 
+
+// ##################################
+// Controls the entire game flow: signals players, reads data, and decides winners
+// ##################################
 int main(int argc, char* argv[]) {
+    // Argument validation
     if (argc != 2) {
         fprintf(stderr, "Usage: %s <config_file>\n", argv[0]);
         return 1;
     }
 
-    read_config(argv[1]);
-    int read_pipes[NUM_PLAYERS][2], write_pipes[NUM_PLAYERS][2];
+    read_config(argv[1]); // Load config
 
+    // Set up pipes so the referee and players can send and receive data
+    int read_pipes[NUM_PLAYERS][2], write_pipes[NUM_PLAYERS][2];
     for (int i = 0; i < NUM_PLAYERS; i++) {
         pipe(read_pipes[i]);
         pipe(write_pipes[i]);
     }
 
+    // Start a separate process for each player and pass them their setup info
     for (int i = 0; i < NUM_PLAYERS; i++) {
         players[i] = fork();
         if (players[i] == 0) {
+            // Close unrelated pipes
             for (int j = 0; j < NUM_PLAYERS; j++) {
                 if (j != i) {
                     close(read_pipes[j][0]); close(read_pipes[j][1]);
@@ -91,6 +79,7 @@ int main(int argc, char* argv[]) {
                 }
             }
 
+            // Format arguments and start the player program using exec
             char pos[10], rfd[10], wfd[10];
             sprintf(pos, "%d", i % TEAM_SIZE);
             sprintf(rfd, "%d", write_pipes[i][0]);
@@ -102,11 +91,13 @@ int main(int argc, char* argv[]) {
         }
     }
 
+    // Close unused pipe ends in referee
     for (int i = 0; i < NUM_PLAYERS; i++) {
         close(read_pipes[i][1]);
         close(write_pipes[i][0]);
     }
 
+    // Set up all counters and timers before starting the game
     sleep(1);
     int score1 = 0, score2 = 0;
     int last_winner = 0;
@@ -114,13 +105,18 @@ int main(int argc, char* argv[]) {
     int prev_energy_t1[TEAM_SIZE] = {0}, prev_energy_t2[TEAM_SIZE] = {0};
     time_t start_time = time(NULL);
 
+    // ##################################
+    // Main Loop
+    // ##################################
     for (int round = 1; round <= config.rounds_to_win; round++) {
         printf("\n=== Round %d ===\n\n", round);
         usleep(500000);
 
+        // Signal players to update energy
         for (int i = 0; i < NUM_PLAYERS; i++) kill(players[i], SIGUSR1);
         sleep(1);
 
+        // Read updated energies
         PlayerStats t1_stats[TEAM_SIZE], t2_stats[TEAM_SIZE];
         int team1_energies[TEAM_SIZE], team2_energies[TEAM_SIZE];
 
@@ -132,20 +128,23 @@ int main(int argc, char* argv[]) {
             team2_energies[i] = t2_stats[i].energy;
         }
 
+        // Assign new positions based on their current energy levels
         int team1_pos[TEAM_SIZE], team2_pos[TEAM_SIZE];
         assign_positions(team1_energies, team1_pos);
         assign_positions(team2_energies, team2_pos);
 
+        // Inform each player of their updated position via pipe
         for (int i = 0; i < TEAM_SIZE; i++) {
             write(write_pipes[i][1], &team1_pos[i], sizeof(int));
             write(write_pipes[i + TEAM_SIZE][1], &team2_pos[i], sizeof(int));
         }
 
+        // Signal players to send effort
         for (int i = 0; i < NUM_PLAYERS; i++) kill(players[i], SIGUSR2);
         sleep(1);
 
+        // Read player efforts
         int total1 = 0, total2 = 0;
-
         for (int i = 0; i < TEAM_SIZE; i++) {
             read(read_pipes[i][0], &t1_stats[i], sizeof(PlayerStats));
             read(read_pipes[i + TEAM_SIZE][0], &t2_stats[i], sizeof(PlayerStats));
@@ -154,6 +153,7 @@ int main(int argc, char* argv[]) {
             total2 += t2_stats[i].effort;
         }
 
+        // Display team stats
         printf("Team 1:\nPlayer | Position | Energy | Effort\n");
         for (int i = 0; i < TEAM_SIZE; i++) {
             char* change = " ";
@@ -186,6 +186,7 @@ int main(int argc, char* argv[]) {
             prev_energy_t2[i] = t2_stats[i].energy;
         }
 
+        // Evaluate team efforts, determine the round winner, and update scores
         printf("\n>> Team 1 Total: %d\t| Team 2 Total: %d\n", total1, total2);
 
         if (total1 >= config.win_threshold && total1 > total2) {
@@ -209,16 +210,18 @@ int main(int argc, char* argv[]) {
             break;
         }
 
+        // Check if the total game time has passed the allowed duration
         time_t current_time = time(NULL);
         int elapsed = (int)(current_time - start_time);
         if (elapsed >= config.game_duration) {
-            printf("\n\u23F0 Game duration of %d seconds reached! Game ends now.\n", config.game_duration);
+            printf("\n⏰ Game duration of %d seconds reached! Game ends now.\n", config.game_duration);
             break;
         }
 
         usleep(500000);
     }
 
+    // Show the final winner (or a tie) after the game ends
     printf("\n=== Game Over ===\n");
     if (score1 > score2)
         printf("\U0001F3C6 Final Winner: Team 1!\n");
@@ -227,6 +230,7 @@ int main(int argc, char* argv[]) {
     else
         printf("\U0001F3C1 Final Result: It's a tie!\n");
 
+    // Terminate all players
     for (int i = 0; i < NUM_PLAYERS; i++) {
         kill(players[i], SIGTERM);
         wait(NULL);
