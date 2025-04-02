e5af3c1310e1333277ceeeaeeb2b3bd940c1a469
Hala Mohammed <halaabed56@gmail.com>
2025-04-03 00:18:35 +0200

    Update header.h

--- include/header.h
+++ include/header.h
@@ -1,1 +1,29 @@
+#ifndef HEADER_H
+#define HEADER_H
 
+// Standard libraries
+#include <stdio.h>
+#include <stdlib.h>
+#include <unistd.h>
+#include <signal.h>
+#include <time.h>
+#include <string.h>
+#include <fcntl.h>
+#include <errno.h>
+#include <limits.h>
+
+// Process & IPC
+#include <sys/types.h>
+#include <sys/stat.h>
+#include <sys/wait.h>
+#include <sys/ipc.h>
+#include <sys/msg.h>
+#include <sys/sem.h>
+#include <sys/shm.h>
+#include <semaphore.h>
+#include <pthread.h>
+
+// OpenGL
+#include <GL/glut.h>
+
+#endif
